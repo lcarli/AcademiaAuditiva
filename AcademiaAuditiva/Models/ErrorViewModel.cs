795efbe24a7d0cf42cd45059d5eb9bf6a2b8ff01
Lucas Humenhuk <lramoscostah@microsoft.com>
2023-08-21 11:45:24 -0400

    First

---
+++ AcademiaAuditiva/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcademiaAuditiva.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
