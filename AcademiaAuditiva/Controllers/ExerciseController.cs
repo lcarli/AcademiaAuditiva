074696eb261a40600dc5171199d571107b0c7c6a
Lucas Humenhuk <lramoscostah@microsoft.com>
2023-08-23 15:48:08 -0400

    Adding GuessQuality

--- AcademiaAuditiva/Controllers/ExerciseController.cs
+++ AcademiaAuditiva/Controllers/ExerciseController.cs
@@ -205,5 +205,66 @@ namespace AcademiaAuditiva.Controllers
 		}
 		#endregion
 
+		#region GuessQuality
+
+		public IActionResult GuessQuality()
+		{
+			int bestScore = _context.Scores
+						   .Where(s => s.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier) && s.Exercise.Name == "GuessQuality")
+						   .OrderByDescending(s => s.BestScore)
+						   .FirstOrDefault()?.BestScore ?? 0;
+
+			ViewBag.BestScore = bestScore;
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult GuessQualityChordsSaveScore(int correctCount, int errorCount)
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+			// Verifique se o usuário está logado
+			if (string.IsNullOrEmpty(userId))
+			{
+				return Json(new { success = false, message = "Usuário não está logado." });
+			}
+
+			// Obtenha o ExerciseId para "GuessQuality"
+			var exercise = _context.Exercises.FirstOrDefault(e => e.Name == "GuessQuality");
+			if (exercise == null)
+			{
+				return Json(new { success = false, message = "Exercício GuessQuality não encontrado." });
+			}
+
+			int currentScore = correctCount - errorCount;
+
+			// Pegue o best score atual para o usuário neste exercício
+			var userBestScoreRecord = _context.Scores
+											  .Where(s => s.UserId == userId && s.ExerciseId == exercise.ExerciseId)
+											  .OrderByDescending(s => s.BestScore)
+											  .FirstOrDefault();
+
+			int userBestScore = userBestScoreRecord != null ? userBestScoreRecord.CorrectCount - userBestScoreRecord.ErrorCount : int.MinValue;
+
+			// Salve apenas se o score atual for melhor que o melhor score do usuário para esse exercício
+			if (currentScore > userBestScore)
+			{
+				_context.Scores.Add(new Score
+				{
+					UserId = userId,
+					ExerciseId = exercise.ExerciseId,
+					CorrectCount = correctCount,
+					ErrorCount = errorCount,
+					BestScore = currentScore
+				});
+				_context.SaveChanges();
+				return Json(new { success = true, message = "Novo recorde!" });
+			}
+
+			return Json(new { success = false, message = "Não superou o recorde anterior." });
+		}
+
+		#endregion
+
 	}
 }
--- AcademiaAuditiva/wwwroot/js/site.js
+++ AcademiaAuditiva/wwwroot/js/site.js
@@ -94,6 +94,105 @@
             }
         }
         return allChords.filter(chord => chord.notes.length === 3); // Filtramos acordes incompletos
+    },
+
+    getAllQualityChords: function () {
+        const allNotes = this.getAllNotes();
+
+        const majorThirds = [4, 3];
+        const minorThirds = [3, 4];
+        const diminishedThirds = [3, 3];
+        const majorSeventh = 11;
+        const minorSeventh = 10;
+        const diminishedSeventh = 9;
+
+        function getChordNotes(rootNote, intervals, seventh = null) {
+            let currentIndex = allNotes.indexOf(rootNote);
+            const chord = [rootNote];
+
+            for (const interval of intervals) {
+                currentIndex += interval;
+                if (currentIndex < allNotes.length) {
+                    chord.push(allNotes[currentIndex]);
+                } else {
+                    return [];
+                }
+            }
+
+            if (seventh !== null) {
+                currentIndex += seventh;
+                if (currentIndex < allNotes.length) {
+                    chord.push(allNotes[currentIndex]);
+                } else {
+                    return [];
+                }
+            }
+
+            return chord;
+        }
+
+        const allChords = [];
+        for (const note of allNotes) {
+            // Para simplificar, não consideramos acordes formados com notas das oitavas 6 e 7
+            if (!note.endsWith('6') && !note.endsWith('7')) {
+                allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
+                allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
+                allChords.push({ type: "diminished", notes: getChordNotes(note, diminishedThirds) });
+                allChords.push({ type: "major7", notes: getChordNotes(note, majorThirds, majorSeventh) });
+                allChords.push({ type: "minor7", notes: getChordNotes(note, minorThirds, minorSeventh) });
+                allChords.push({ type: "diminished7", notes: getChordNotes(note, diminishedThirds, diminishedSeventh) });
+            }
+        }
+        return allChords.filter(chord => chord.notes.length >= 3); // Filtramos acordes incompletos
+    },
+
+    getAllQualityChordsWithinOctave: function (octave) {
+        const allNotes = this.getAllNotes();
+
+        const majorThirds = [4, 3];
+        const minorThirds = [3, 4];
+        const diminishedThirds = [3, 3];
+        const majorSeventh = 11;
+        const minorSeventh = 10;
+        const diminishedSeventh = 9;
+
+        function getChordNotes(rootNote, intervals, seventh = null) {
+            let currentIndex = allNotes.indexOf(rootNote);
+            const chord = [rootNote];
+
+            for (const interval of intervals) {
+                currentIndex += interval;
+                if (currentIndex < allNotes.length) {
+                    chord.push(allNotes[currentIndex]);
+                } else {
+                    return [];
+                }
+            }
+
+            if (seventh !== null) {
+                currentIndex += seventh;
+                if (currentIndex < allNotes.length) {
+                    chord.push(allNotes[currentIndex]);
+                } else {
+                    return [];
+                }
+            }
+
+            return chord;
+        }
+
+        const allChords = [];
+        for (const note of allNotes) {
+            if (note.endsWith(octave.toString())) {
+                allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
+                allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
+                allChords.push({ type: "diminished", notes: getChordNotes(note, diminishedThirds) });
+                allChords.push({ type: "major7", notes: getChordNotes(note, majorThirds, majorSeventh) });
+                allChords.push({ type: "minor7", notes: getChordNotes(note, minorThirds, minorSeventh) });
+                allChords.push({ type: "diminished7", notes: getChordNotes(note, diminishedThirds, diminishedSeventh) });
+            }
+        }
+        return allChords.filter(chord => chord.notes.length >= 3); // Filtramos acordes incompletos
     }
 
 };
