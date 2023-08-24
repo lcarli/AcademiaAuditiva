aa00a71dd8a3a3364f0e334ee38b409710d98794
Lucas Humenhuk <lucas.decarli@outlook.com>
2023-08-23 20:29:26 -0400

    Delete node_modules directory

--- node_modules/@tonejs/piano/build/piano/Util.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function noteToMidi(note: string): number;
-export declare function midiToNote(midi: number): string;
-export declare function randomBetween(low: number, high: number): number;
