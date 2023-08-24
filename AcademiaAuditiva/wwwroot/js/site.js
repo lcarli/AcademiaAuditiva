ba2c915d3356f8b4892b8fc68358b267c93f5d23
Lucas Humenhuk <lramoscostah@microsoft.com>
2023-08-23 21:59:52 -0400

    Create method GetHarmonicField

--- AcademiaAuditiva/wwwroot/js/site.js
+++ AcademiaAuditiva/wwwroot/js/site.js
@@ -34,69 +34,7 @@
         return Object.keys(this._generateNoteUrls());
     },
 
-    getAllChords: function () {
-        const allNotes = this.getAllNotes();
-        const majorThirds = [4, 3];
-        const minorThirds = [3, 4];
-
-        function getChordNotes(rootNote, intervals) {
-            let currentIndex = allNotes.indexOf(rootNote);
-            const chord = [rootNote];
-
-            for (const interval of intervals) {
-                currentIndex += interval;
-                if (currentIndex < allNotes.length) { 
-                    chord.push(allNotes[currentIndex]);
-                } else {
-                    return [];
-                }
-            }
-            return chord;
-        }
-
-        const allChords = [];
-        for (const note of allNotes) {
-            // Para simplificar, não consideramos acordes formados com notas das oitavas 6 e 7
-            if (!note.endsWith('6') && !note.endsWith('7')) {
-                allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
-                allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
-            }
-        }
-        return allChords.filter(chord => chord.notes.length === 3); // Filtramos acordes incompletos
-    },
-
-    getAllChordsWithinOctave: function (octave) {
-        const allNotes = this.getAllNotes();
-        const majorThirds = [4, 3];
-        const minorThirds = [3, 4];
-
-        function getChordNotes(rootNote, intervals) {
-            let currentIndex = allNotes.indexOf(rootNote);
-            const chord = [rootNote]; 
-
-            for (const interval of intervals) {
-                currentIndex += interval;
-                if (currentIndex < allNotes.length) {
-                    chord.push(allNotes[currentIndex]);
-                } else {
-                    return [];
-                }
-            }
-            return chord;
-        }
-
-        const allChords = [];
-        for (const note of allNotes) {
-            // Verificamos se a nota atual é da oitava especificada
-            if (note.endsWith(octave.toString())) {
-                allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
-                allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
-            }
-        }
-        return allChords.filter(chord => chord.notes.length === 3); // Filtramos acordes incompletos
-    },
-
-    getAllQualityChords: function () {
+    getAllChords: function (octave = null) {
         const allNotes = this.getAllNotes();
 
         const majorThirds = [4, 3];
@@ -133,66 +71,67 @@
 
         const allChords = [];
         for (const note of allNotes) {
-            // Para simplificar, não consideramos acordes formados com notas das oitavas 6 e 7
-            if (!note.endsWith('6') && !note.endsWith('7')) {
-                allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
-                allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
-                allChords.push({ type: "diminished", notes: getChordNotes(note, diminishedThirds) });
-                allChords.push({ type: "major7", notes: getChordNotes(note, majorThirds, majorSeventh) });
-                allChords.push({ type: "minor7", notes: getChordNotes(note, minorThirds, minorSeventh) });
-                allChords.push({ type: "diminished7", notes: getChordNotes(note, diminishedThirds, diminishedSeventh) });
+            if (octave != null) {
+                if (note.endsWith(octave.toString())) {
+                    allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
+                    allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
+                    allChords.push({ type: "diminished", notes: getChordNotes(note, diminishedThirds) });
+                    allChords.push({ type: "major7", notes: getChordNotes(note, majorThirds, majorSeventh) });
+                    allChords.push({ type: "minor7", notes: getChordNotes(note, minorThirds, minorSeventh) });
+                    allChords.push({ type: "diminished7", notes: getChordNotes(note, diminishedThirds, diminishedSeventh) });
+                }
+            }
+            else {
+                if (!note.endsWith('7')) {
+                    allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
+                    allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
+                    allChords.push({ type: "diminished", notes: getChordNotes(note, diminishedThirds) });
+                    allChords.push({ type: "major7", notes: getChordNotes(note, majorThirds, majorSeventh) });
+                    allChords.push({ type: "minor7", notes: getChordNotes(note, minorThirds, minorSeventh) });
+                    allChords.push({ type: "diminished7", notes: getChordNotes(note, diminishedThirds, diminishedSeventh) });
+                }
             }
         }
         return allChords.filter(chord => chord.notes.length >= 3); // Filtramos acordes incompletos
     },
 
-    getAllQualityChordsWithinOctave: function (octave) {
+    getHarmonicField: function () {
+        const allChords = this.getAllChords();
         const allNotes = this.getAllNotes();
 
-        const majorThirds = [4, 3];
-        const minorThirds = [3, 4];
-        const diminishedThirds = [3, 3];
-        const majorSeventh = 11;
-        const minorSeventh = 10;
-        const diminishedSeventh = 9;
+        const majorHarmonicField = ["0-major", "2-minor", "4-minor", "5-major", "7-major", "9-minor", "11-dim"];
+        const minorHarmonicField = ["0-minor", "2-dim", "4-major", "5-menor", "7-menor", "9-major", "11-major"];
 
-        function getChordNotes(rootNote, intervals, seventh = null) {
+        function getHarmonicNotes(rootNote, harmonicField) {
             let currentIndex = allNotes.indexOf(rootNote);
-            const chord = [rootNote];
+            const chord = [];
 
-            for (const interval of intervals) {
-                currentIndex += interval;
-                if (currentIndex < allNotes.length) {
-                    chord.push(allNotes[currentIndex]);
-                } else {
-                    return [];
+            if (harmonicField == 'major') {
+                for (const n of majorHarmonicField) {
+                    const index = parseInt(n.split('-')[0]);
+                    const quality = n.split('-')[1];
+                    const test = allChords.filter(chord => chord.type == quality && chord.notes[0] == allNotes[currentIndex + index]);
+                    chord.push(test)
                 }
             }
-
-            if (seventh !== null) {
-                currentIndex += seventh;
-                if (currentIndex < allNotes.length) {
-                    chord.push(allNotes[currentIndex]);
-                } else {
-                    return [];
+            else {
+                for (const n of minorHarmonicField) {
+                    const index = parseInt(n.split('-')[0]);
+                    const quality = n.split('-')[1];
+                    const test = allChords.filter(chord => chord.type == quality && chord.notes[0] == allNotes[currentIndex + index]);
+                    chord.push(test)
                 }
             }
 
             return chord;
         }
 
-        const allChords = [];
+        const returnChords = [];
         for (const note of allNotes) {
-            if (note.endsWith(octave.toString())) {
-                allChords.push({ type: "major", notes: getChordNotes(note, majorThirds) });
-                allChords.push({ type: "minor", notes: getChordNotes(note, minorThirds) });
-                allChords.push({ type: "diminished", notes: getChordNotes(note, diminishedThirds) });
-                allChords.push({ type: "major7", notes: getChordNotes(note, majorThirds, majorSeventh) });
-                allChords.push({ type: "minor7", notes: getChordNotes(note, minorThirds, minorSeventh) });
-                allChords.push({ type: "diminished7", notes: getChordNotes(note, diminishedThirds, diminishedSeventh) });
-            }
+            returnChords.push({ type: "major", notes: getHarmonicNotes(note, 'major') });
+            returnChords.push({ type: "minor", notes: getHarmonicNotes(note, 'minor') });
         }
-        return allChords.filter(chord => chord.notes.length >= 3); // Filtramos acordes incompletos
+        return returnChords.filter(chord => chord.notes.length >= 3); // Filtramos acordes incompletos
     }
 
 };
